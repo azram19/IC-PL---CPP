de24031a2aa775df4a8c8489db86ffad4137291d
Piotr Bar <baru02@gmail.com>
2011-06-18 17:30:12 +0100

    aaa

--- printingParrot.cpp
+++ printingParrot.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-enum vehicleType {Lorry, PetrolCar, DieselCar, Bus};
-enum messageType {Registration, BusIn, BusOut, Entrance, SetLimit, SetCharge};
+//enum vehicleType {Lorry, PetrolCar, DieselCar, Bus};
+//enum messageType {Registration, BusIn, BusOut, Entrance, SetLimit, SetCharge};
 
 template <class V, class M>
 
 void print_message(V v, M m){
 	switch(m){
-		//Reg - cases for n-axles lorries and n-ppcm diesels
-		case Registration: cout<<"A "<<v.get_type()<<" ("<<v.get_name()<<") has been registered"<<endl;
+		case Registration: cout<<"A ";
+			if(typeid(v)==typeid(Lorry)){cout<<v.get_ppcm()<<"-ppcm ";}
+			if(typeid(v)==typeid(dieselCar)){cout<<v.get_axles()<<"-axle ";}
+			cout<<v.get_type()<<" ("<<v.get_name()<<") has been registered"<<endl;
 			break;
 		case BusIn: cout<<v.get_passengers()<<" passengers board the bus ("<<v.get_name()<<"), so "<<v.get_total_passengers()
 			<<"passengers are on board"<<endl;
@@ -19,8 +21,10 @@ void print_message(V v, M m){
 		case BusOut: cout<<v.get_passengers()<<" passengers leave the bus ("<<v.get_name()<<"), leaving "<<v.total_passengers()
 			<<" passengers on board"<<endl;
 			break;
-		// Entrance - cases for n-axles lorries and n-ppcm diesels
-		case Entrance:cout<<"The "<<v.get_type()<<" ("<<v.get_name()<<") enters on "<<v.get_date()<<" at "<<v.get_time()<<" hours"<<endl;
+		case Entrance:cout<<"The ";
+			if(typeid(v)==typeid(Lorry)){cout<<v.get_ppcm()<<"-ppcm ";}
+			if(typeid(v)==typeid(dieselCar)){cout<<v.get_axles()<<"-axle ";}
+			cout<<v.get_type()<<" ("<<v.get_name()<<") enters on "<<v.get_date()<<" at "<<v.get_time()<<" hours"<<endl;
 			if(v.wascharged()){
 				cout<<"The vehicle has already been charged today ; no action is taken"<<endl;}
 			else if(v.get_type() = Bus and v.get_passengers()>20){
@@ -38,6 +42,3 @@ void print_message(V v, M m){
 
 }
 
-int main(){
-	return 0;
-}
